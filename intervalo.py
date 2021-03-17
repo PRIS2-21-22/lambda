c87ce2f55984fda563f7f5030b989961939bc12f
Jose <jmm497@inlumine.ual.es>
2021-03-17 20:22:16 +0100

    Añadida función antes

--- intervalo.py
+++ intervalo.py
@@ -1,14 +1,26 @@
 class intervalo:
-    def __init__(self, x , y):
+    
+    def __init__(self,x,y):
         self.x = x
         self.y = y
 
+    def antes(self):       
+        if (self.x < self.y): 
+            return True
+        return False
+
+    def __str__(self):
+        return "x: {}, y: {}".format(self.x, self.y)
+
+print("Probemos con un x < y")
+prueba = intervalo(x=1, y=2)
+print(prueba)
+print(prueba.antes())
+print("Probemos con un y < x")
+prueba2 = intervalo(x=4, y=2)
+print(prueba2)
+print(prueba2.antes())
 
 
 
-
-
-
-
-
-print("Hello world 2")
+#print("Hello world 2")
